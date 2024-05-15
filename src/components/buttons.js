9acea808552f2a6ce122e6a021806fd8fdda27ab
Henry Lu <henry11241@gmail.com>
2024-05-15 18:31:08 +0800

    Project init with basic layout

---
+++ src/components/buttons.js
@@ -0,0 +1,9 @@
+export default function Buttons() {
+  return (
+    <div className="buttons flex flex-row">
+      <button className="pt-[8px] pb-[6px] px-[18px] rounded bg-gray-700 text-white ml-auto">
+        New quote
+      </button>
+    </div>
+  )
+}
---
+++ src/components/quoteAuthor.js
@@ -0,0 +1,3 @@
+export default function QuoteAuthor() {
+  return <div className="quote-author flex justify-end">-</div>
+}
