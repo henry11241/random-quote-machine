dca06d6fea72d5d125da16a619eee26376af2227
Henry Lu <henry11241@gmail.com>
2024-05-17 15:40:06 +0800

    Display fetched data and rewrite fadeIn animate logic

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,11 @@
 import Buttons from '@/components/buttons'
 import QuoteAuthor from '@/components/quoteAuthor'
 import QuoteText from '@/components/quoteText'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const [color, setColor] = useState('#374151')
-  const [animate, setAnimate] = useState(false)
-
+  
   const calcLuminance = (hex) => {
     const m = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
     const colors = [parseInt(m[1], 16), parseInt(m[2], 16), parseInt(m[3], 16)]
@@ -33,19 +32,50 @@ export default function Home() {
       }
     } while (calcLuminance(color) >= 0.179)
     setColor(color)
-    setAnimate(true)
-    setTimeout(() => {
-      setAnimate(false)
-    }, 2000)
+    fadeIn()
+    getRandomQuote()
   }
+
+  const fadeIn = () => {
+    const quoteText = document.querySelector('.quote-text')
+    const quoteAuthor = document.querySelector('.quote-author')
+    quoteText.classList.remove('fadeIn')
+    quoteAuthor.classList.remove('fadeIn')
+    requestAnimationFrame((time) => {
+      requestAnimationFrame((time) => {
+        quoteText.classList.add('fadeIn')
+        quoteAuthor.classList.add('fadeIn')
+      })
+    })
+  }
+
+  const getRandomQuote = async () => {
+    try {
+      const res = await fetch('https://api.quotable.io/quotes/random')
+      const [data] = await res.json()
+      const quote = document.getElementById('quote')
+      const quoteAuthor = document.getElementById('quote-author')
+      const { content, author } = data
+      quote.innerText = content
+      quoteAuthor.innerText = author
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fadeIn()
+    getRandomQuote()
+  }, [])
 
   return (
     <main
       className={`flex flex-col min-h-screen items-center justify-center bg-gray-700 transition ease-in-out duration-[2000ms]`}
     >
-      <div className="flex flex-col card bg-white min-h-[300px] w-[550px] px-[40px] py-[50px] rounded justify-between">
-        <QuoteText color={color} animate={animate} />
-        <QuoteAuthor color={color} animate={animate} />
+      <div className="flex flex-col card bg-white min-h-[300px] w-[550px] px-[40px] py-[50px] rounded justify-between gap-8">
+        <QuoteText color={color} />
+        <QuoteAuthor color={color} />
         <Buttons color={color} onClick={handleClick} />
       </div>
       <style jsx>{`
--- src/components/quoteAuthor.js
+++ src/components/quoteAuthor.js
@@ -1,11 +1,17 @@
-export default function QuoteAuthor({ color, animate }) {
+import { Satisfy } from 'next/font/google'
+
+const satisfy = Satisfy({ weight: '400', subsets: ['latin'] })
+
+export default function QuoteAuthor({ color }) {
   return (
     <div
-      className={`quote-author flex justify-end transition ease-in-out duration-[2000ms] ${
-        animate ? 'fadeIn' : ''
-      }`}
+      className={`quote-author flex justify-end transition ease-in-out duration-[2000ms]`}
     >
       -
+      <span
+        id="quote-author"
+        className={`text-l before:ml-1 ${satisfy.className}`}
+      ></span>
       <style jsx>{`
         .quote-author {
           color: ${color};
